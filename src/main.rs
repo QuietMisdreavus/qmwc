8c7cd6ae9ba06e542852f5e22b8d08e5198a03ad
QuietMisdreavus <victoria@quietmisdreavus.net>
2020-06-01 10:13:24 -0600

    add --set-dir command to save wallpaper dir

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+
 fn args() -> clap::App<'static, 'static> {
     clap::App::new("QuietMisdreavus Wallpaper Cycler")
         .author("(c) 2020 QuietMisdreavus")
@@ -26,12 +30,40 @@ fn args() -> clap::App<'static, 'static> {
             .help("Sets the directory used to source wallpaper"))
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = args().get_matches();
 
     if let Some(wall_dir) = args.value_of("set-dir") {
         println!("Setting wallpaper directory to {}", wall_dir);
+        set_wallpaper_dir(wall_dir)?;
     } else {
+        let _dir = get_wallpaper_dir()?;
         println!("TODO");
     }
+
+    Ok(())
+}
+
+fn get_config_file() -> io::Result<PathBuf> {
+    let mut c = dirs::config_dir().expect("could not locate XDG Config folder");
+    c.push("qmwc");
+    fs::create_dir_all(&c)?;
+    c.push("config.txt");
+    Ok(c)
+}
+
+fn set_wallpaper_dir(dir: &str) -> io::Result<()> {
+    let f = get_config_file()?;
+    fs::write(&f, dir)?;
+    Ok(())
+}
+
+fn get_wallpaper_dir() -> io::Result<PathBuf> {
+    let f = get_config_file()?;
+    if !f.exists() {
+        eprintln!("Wallpaper directory not set. Call this program with `--set-dir` first to set the directory.");
+        return Err(io::Error::new(io::ErrorKind::NotFound, "configuration not found"));
+    }
+    let dir = fs::read_to_string(&f)?;
+    Ok(PathBuf::from(dir))
 }
